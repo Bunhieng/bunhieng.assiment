4b67e26e42ee983ad3d8b999e842039884d67259
USER <Bunhienghakk@gmail.com>
2021-08-08 09:50:12 +0700

    Initial commit

---
+++ src/excecute/Main.java
@@ -0,0 +1,18 @@
+package excecute;
+import math_quize.SUM;
+import util.Util;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int x = (int) (math.random()*10);
+        int y = (int) (math.random()*10);
+
+        SUM sumlevel1=new SUM();
+        Util.showMessageCal(x, y, sign " + ");
+        int studentAnser = new Scaner(System.in).nextInt();
+        system.out.println(sumlevel1.sum(x, y) studentAnswer));
+//          Util.showSum(x, y, sumlevel1.sum(x, y));
+    }
+}
---
+++ src/model/TV.java
@@ -0,0 +1,7 @@
+package model;
+
+public class TV {
+    private int chanel = 1;
+    private int volumelevel = 1;
+    private boolean on;
+}
